90a3f9fa8be0b5e93d6917c217ff9bc032556a72
latedude2 <simasc10@gmail.com>
2021-04-24 20:02:02 +0200

    Add gallery and first project

---
+++ initializeGallery.js
@@ -0,0 +1,21 @@
+mibreitGallery.createGallery({
+
+    // slideshow container
+    slideshowContainer: ".image-gallery",
+  
+    // thumb view container
+    thumbviewContainer: ".mibreit-thumbview",
+  
+    // image title container
+    titleContainer: "#full-gallery-title",
+  
+    // determine whether to enable fullscreen mode
+    allowFullscreen: !1,
+  
+    // image preloader
+    preloadLeftNr: 2,
+    preloadRightNr: 3,
+
+
+  })
+  
