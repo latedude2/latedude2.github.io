848f03925ebc303546d6c7e0035909333a4c0d55
latedude2 <simasc10@gmail.com>
2021-06-02 20:34:22 +0200

    Add vue

---
+++ showProjects.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        projects: [
+          { title: '🎨 Ink Busters - Multiplayer FPS Game (2021)' , link: '/projects/dynamicDifficultyAdjustment.html' },
+          { title: '🔥 Wildfire Rush - Action/Puzzle Game (2021)', link: '/projects/wildfireRush.html' },
+          { title: '🐝 Game Navigational Aids Research - Adventure Game (2020)', link: '/projects/gameNavigationalAids.html' },
+          { title: '🥳 Party LEDs - Arduino project (2020)', link: '/projects/partyLeds.html' },
+          { title: '♠️ Poker assistance app - Image Processing project (2019)', link: '/projects/pokerApp.html' },
+          { title: '🇩🇰 Language Learning and Narrative Research - Android story based game (2019)', link: '/projects/languageGame.html' },
+          { title: '🥚 EggMission - 2D Unity Arcade Driving game (2018)', link: '/projects/eggMission.html' },
+          { title: '♟️ Sorex - C++ .NET Chess game (2016)', link: '/projects/sorex.html' }
+        ]
+      }
+  })
